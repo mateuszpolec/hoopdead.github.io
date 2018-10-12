402e106e8dc6bcfc7267cab749849aa1702ba802
HoopDead <polec0@wp.pl>
2018-10-12 22:28:11 +0200

    reservation

--- portfolio/hackathon/js/payment.js
+++ portfolio/hackathon/js/payment.js
@@ -1,13 +1,20 @@
 function showGym()
 {
-    document.getElementById("gym").setAttribute("style", "display:block");
+    document.getElementById("gym").setAttribute("style", "display: block");
     document.getElementById("swim").setAttribute("style", "display: none");
-    document.getElementById("squash").setAttribute("style", "display: none");
+    document.getElementById("premium").setAttribute("style", "display: none");
 }
 
 function showSwim()
 {
-    document.getElementById("gym").setAttribute("style", "display:none");
+    document.getElementById("gym").setAttribute("style", "display: none");
     document.getElementById("swim").setAttribute("style", "display: block");
-    document.getElementById("squash").setAttribute("style", "display: none");  
+    document.getElementById("premium").setAttribute("style", "display: none");  
+}
+
+function showPremium()
+{
+    document.getElementById("gym").setAttribute("style", "display: none");
+    document.getElementById("swim").setAttribute("style", "display: none");
+    document.getElementById("premium").setAttribute("style", "display: block");  
 }
