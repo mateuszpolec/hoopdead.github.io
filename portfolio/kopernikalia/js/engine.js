15552e4a1ef61521c7958475af875b8fe0e53962
HoopDead <polec0@wp.pl>
2018-10-12 08:41:35 +0200

    Hackathon +1

---
+++ portfolio/kopernikalia/js/engine.js
@@ -0,0 +1,21 @@
+(function($) {
+    "use strict";
+
+  
+
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#main").offset().top > 10) {
+        $("#main").addClass("navbar-background");
+      } else {
+        $("#main").removeClass("navbar-background");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+
+ 
+  })(jQuery); // End of use strict
