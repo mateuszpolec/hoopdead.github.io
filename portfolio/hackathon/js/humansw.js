030beb33ea976ba2703145981002abd7a6c29d6d
HoopDead <polec0@wp.pl>
2018-10-13 02:26:26 +0200

    idk a lot of new stuff

---
+++ portfolio/hackathon/js/humansw.js
@@ -0,0 +1,28 @@
+function bodyGot()
+{
+    document.getElementById("right").setAttribute("style", "display: none");
+    document.getElementById("left").setAttribute("style", "display: none");   
+    document.getElementById("czlowiek").setAttribute("style", "display: block")
+    document.getElementById("x").setAttribute("style", "display: block");
+    document.getElementById("legs").setAttribute("style", "display: block");
+
+}
+
+function bodyOwn()
+{
+    document.getElementById("x").setAttribute("style", "display: block");
+    document.getElementById("right").setAttribute("style", "display: none");
+    document.getElementById("left").setAttribute("style", "display: none");
+    document.getElementById("czlowiek").setAttribute("style", "display: block")
+}
+
+function legShow()
+{
+    document.getElementById("leg1").setAttribute("style", "display: block");
+    document.getElementById("leg2").setAttribute("style", "display: block");
+    document.getElementById("leg3").setAttribute("style", "display: block");
+    document.getElementById("leg4").setAttribute("style", "display: block");
+    document.getElementById("clock").setAttribute("style", "display: block");
+
+}
+
