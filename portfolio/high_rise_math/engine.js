0ed3bd4eedcb1f98290a3ad83651c3af63fc560b
hoopdead <polec0@wp.pl>
2019-08-06 18:37:57 +0200

    Update for Math Game - bugfixes, new featuers - timer, points, background change on correct answer/incorrect answer.

--- portfolio/high_rise_math/engine.js
+++ portfolio/high_rise_math/engine.js
@@ -1,40 +1,43 @@
 var level = 1;
 var operations = ["+", "-", "*"]
 var answer = 0;
-var points = 0;
 var timer = false;
+var initial;
 
 function create_math()
 {
     if(!timer)
     {
         timer = true;
-        alert("Start");
+        invocation();
     }
     var first_number = Math.floor((Math.random() * level*2) + 1);
     var second_number = Math.floor((Math.random() * level*2) + 1);
     var operation = Math.floor((Math.random() * 3) + 0);
-    document.getElementById("firstnumber").innerHTML = first_number;
-    document.getElementById("operation").innerHTML = operations[operation];
-    document.getElementById("secondnumber").innerHTML = second_number;
+    $("#firstnumber").html(first_number);
+    $("#operation").html(operations[operation]);
+    $("#secondnumber").html(second_number);
     $("#demo").css("display", "none");
     $("#game").css("display", "flex");
-    switch(operation)
+    result(first_number, operation, second_number);
+}
+
+function result(first_number, operation, second_number)
+{
+    if(operation == 0)
     {
-        case 0:
-            answer = first_number + second_number;
-            document.getElementById("answer").innerHTML = answer;
-            break;
-        case 1:
-            answer = first_number - second_number;
-            document.getElementById("answer").innerHTML = answer;
-            break;
-
-        case 2:
-            answer = first_number * second_number;
-            document.getElementById("answer").innerHTML = answer;
-            break;
-
+        answer = first_number + second_number;
+        $("#answer").html(answer);
+    }
+    else if(operation == 1)
+    {
+        answer = first_number - second_number;
+        $("#answer").html(answer);
+    }
+    else if(operation == 2)
+    {
+        answer = first_number * second_number;
+        $("#answer").html(answer);
     }
 }
 
@@ -57,11 +60,39 @@ function submit_answer()
     {
         document.getElementById("userInput").value = ""; 
         create_math();
+        $("#game").css("background-color", "#238c24");
+        setTimeout(function() {
+            $("#game").css("background-color", "#00BBFF");
+        }, 350);
     }
     else
     {
+        $("#game").css("background-color", "#8f0e0e");
+        setTimeout(function() {
+            $("#game").css("background-color", "#00BBFF");
+        }, 350);
     }
 }
-setTimeout(function start_timer() {
-    console.log(level);
-}, 60000);
+
+function reset()
+{
+    level = 1;
+    answer = 0;
+    points = 0;
+    timer = false;
+    $("#final").css("display", "none");
+    create_math();
+    clearTimeout( initial );
+    invocation();
+}
+
+
+function invocation() {
+    initial = window.setTimeout( 
+    function() {
+        $("#game").css("display", "none");
+        $("#score").html(level * 60);
+        $("#final").css("display", "flex");    
+    }, 5000);
+}
+
