b3f036899df59c3e46f7d7ec19ffba06f8373f47
hoopdead <polec0@wp.pl>
2019-09-08 16:58:58 +0200

    Second update - add about me section, and bugfix
    A lot of new feature. New section - about me, bugfix with box that allow you to go to another section,
    probably bugfix with overflow on smaller devies

--- assets/engine.js
+++ assets/engine.js
@@ -1,4 +1,5 @@
 let tab_id = "tab";
+let about = false;
 
 $(".showproject").click(function() {
   $("#main").css("display", "none");
@@ -11,4 +12,20 @@ $(".showproject").click(function() {
     $("#scene").css("display", "none");
     $("#scene").css("z-index", "0");
   }, 2000);
+});
+
+$(".showabout").click(function() {
+  if (about) {
+    console.log("You are already here!");
+  } else {
+    console.log("About is test");
+    $("#scene2").css("display", "flex");
+    $("#about").css("display", "flex");
+    $("#content").css("display", "none");
+    about = true;
+    setTimeout(function() {
+      $("#scene2").css("display", "none");
+      $("#scene2").css("z-index", "0");
+    }, 2000);
+  }
 });
